7330caf4c027f36513cbb220b2d468f281fcb09b
Hicham Kaddouri <hkaddour@e2r5p5.1337.ma>
2023-03-21 15:19:46 +0000

    almost finish

--- Module_09/ex02/PmergeMe.cpp
+++ Module_09/ex02/PmergeMe.cpp
@@ -6,53 +6,88 @@
 /*   By: hkaddour <hkaddour@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 11:17:25 by hkaddour          #+#    #+#             */
-/*   Updated: 2023/03/20 18:34:19 by hkaddour         ###   ########.fr       */
+/*   Updated: 2023/03/21 15:19:09 by hkaddour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-//std::vector<int>	insert_sort(int len, char **data)
-std::vector<int>	insert_sort(int len, int *nbr)
+void	fill_data(std::vector<int> sort, \
+		std::pair<std::vector<int>, std::vector<int> > &tmp, std::pair<int, int> size)
 {
-	std::vector<int>	sort;
-
-
-	for (int i = 0; i < len; i++)
-		sort.push_back(nbr[i]);
-
-	//int	hld;
-	int	j;
-	//std::vector<int>::iterator	itr = sort.begin();
-	for (int	i = 1; i < len; i++)
-	{
-		j = i;
-		//while (*itr > *(itr + 1))
-		//for (int	j = i; sort[j - 1] > sort[j]; j++)
-		while (sort[j - 1] > sort[j])
-		{
-			std::swap(sort[j - 1], sort[j]);
-			j--;
-		}
-	}
-
-	//for (unsigned long i = 0; i < sort.size(); i++)
-
-	std::sort(sort.begin(), sort.end());
-	for (unsigned long i = 0; i < sort.size(); i++)
-		std::cout << sort[i] << std::endl;
-	return (sort);
+	int	i;
+	for (i = 0; i < size.first; i++)
+		tmp.first.push_back(sort[i]);
+	for (; i < static_cast<int>(sort.size()); i++)
+		tmp.second.push_back(sort[i]);
 }
 
-//void	sorting(int len, char **data)
+void	sort_that_shit(std::vector<int> sort, std::pair<std::vector<int>, std::vector<int> > tmp)
+{
+	for (int	i = 0; i < sort.size(); i++)
+	{
+		//for ()
+	}
+	//here finish the algo it's easy senior developers don't code anymore!!
+}
+
+void	merge_sort(std::vector<int> sort)
+{
+	std::pair<int, int> size;
+	std::pair<std::vector<int>, std::vector<int> > tmp;
+
+	if (sort.size() < 2)
+		return ;
+	size.first = sort.size() / 2;
+	size.second = sort.size() - size.first;
+	fill_data(sort, tmp, size);
+	std::vector<int>::iterator	itr;
+	std::cout << "start" << std::endl;
+	for (itr = tmp.first.begin(); itr != tmp.first.end(); itr++)
+		std::cout << *itr << std::endl;
+
+	std::cout << "encore" << std::endl;
+
+	for (itr = tmp.second.begin(); itr != tmp.second.end(); itr++)
+		std::cout << *itr << std::endl;
+	std::cout << "end" << std::endl;
+
+	merge_sort(tmp.first);
+	merge_sort(tmp.second);
+	sort_that_shit(sort, tmp);
+	
+
+
+	//std::vector<int>::iterator	itr;
+	//for (itr = tmp.first.begin(); itr < tmp.first.end(); itr++)
+	//	std::cout << *itr << std::endl;
+
+	//std::cout << "encore" << std::endl;
+
+	//for (itr = tmp.second.begin(); itr < tmp.second.end(); itr++)
+	//	std::cout << *itr << std::endl;
+	//exit(0);
+
+	//std::cout << sort.size() << std::endl;
+	//std::cout << size.first << std::endl;
+	//std::cout << size.second << std::endl;
+}
+
 void	sorting(int len, int *nbr)
 {
 	std::cout << "Before:";
 	for (int i = 0; i < len; i++)
 		std::cout << " " << nbr[i];
 	std::cout << std::endl;
-	insert_sort(len, nbr);
-	//merge_sort(len, nbr);
+	std::vector<int>	sort;
+	for (int i = 0; i < len; i++)
+		sort.push_back(nbr[i]);
+	merge_sort(sort);
+	std::vector<int>::iterator	itr;
+	std::cout << "After:";
+	for (itr = sort.begin(); itr < sort.end(); itr++)
+		std::cout << " " << *itr;
+	std::cout << std::endl;
 }
 
 int	*parsing(int len, char **data)
